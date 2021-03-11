bd9f56737802f66efd3ea6767d75162cbbbbcba6
BorisovVladislav <borisovvlad.spb@gmail.com>
2021-03-11 15:41:19 +0300

    опять разделил на отдельные классы, далее сделаю классы MorningSallaring и ему подобные синглтонами

---
+++ src/main/java/MorningSallaring.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MorningSallaring {
+    private Map<Date, Double> morningSallaring = new LinkedHashMap<>();
+
+    public Map<Date, Double> getMorningSallaring() {
+        return morningSallaring;
+    }
+}
